3d7d5352a0aba4d1786f0e37417d84348e28e532
Axel Preiti Tasat <axel.preitit@gmail.com>
2025-01-22 17:18:11 -0300

    chore: remove unused local variable

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,5 +2,4 @@ locals {
   scripts_path = "${path.module}/scripts"
   ec2_ami = "ami-080e1f13689e07408" # Ubuntu 22.04 LTS (HVM), SDD Volume Type
   ec2_user = "ubuntu"
-  private_key = file(split(".pub", var.ec2_key_file)[0])
 }
