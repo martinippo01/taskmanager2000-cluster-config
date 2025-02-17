d50b2bc35a613dfbac76061d7082beb18a4984ba
martinippo01 <martinippo@gmail.com>
2025-02-17 17:33:19 -0300

    add script to add the workers to the cluster after terraform init

--- init.sh
+++ init.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+# Validate that the correct number of arguments is passed
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <file with cluster ip> <file with workers ips>"
+  echo "Usage: $0 <file with master ip> <file with worker ips>"
   exit 1
 fi
+
+# Validate if SSH key exists
 if [ ! -f ./id_ed25519 ]; then
     echo "Error: SSH key file ./id_ed25519 not found!"
     exit 1
 fi
 
-if [ $(wc -l < "$1") -ne 1 ]; then
-    echo "Error: The file $1 must contain exactly one line."
-    exit 1
-fi
+# Read the master IP and worker IPs
+MASTER_IP=$(<"$1")
+WORKER_IPS=$(<"$2")
 
-if [ $(wc -l < "$2") -lt 1 ]; then
-    echo "Error: The file $2 must contain one or more lines."
-    exit 1
-fi
+# Check if the master node is ready
+echo "==============Checking if the master node is ready==============="
+echo "---$MASTER_IP---"
+ssh -i ./id_ed25519 ubuntu@$MASTER_IP -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu "kubectl get nodes"
 
-join_command="sudo $(ssh -i ./id_ed25519 ubuntu@$(cat $1) -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu "sudo kubeadm token create --print-join-command")"
+# Get the join command from the master node
+echo "========================Getting join command======================"
+join_command="sudo $(ssh -i ./id_ed25519 ubuntu@$MASTER_IP -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu 'sudo kubeadm token create --print-join-command')"
 
-while IFS= read -r worker_ip; do
+# Split the worker IPs and iterate over them
+echo "==============Running the join command on each worker=============="
+for worker_ip in $WORKER_IPS; do
+    echo "---$worker_ip---"
     ssh -i ./id_ed25519 ubuntu@$worker_ip -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu "$join_command"
-done < "$2"
+done
 
-ssh -i ./id_ed25519 ubuntu@$(cat $1) -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu "kubectl get nodes"
+# Verify the nodes again after joining
+echo "==============Checking nodes after joining workers==============="
+ssh -i ./id_ed25519 ubuntu@$MASTER_IP -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o IdentitiesOnly=yes -o User=ubuntu "kubectl get nodes"
